77bbb3a0462b2cc8fc5e1691bf5fb585021ab41b
boody <abdoarfat2006@gmail.com>
2025-07-29 19:07:42 +0300

    ci: add sign middelwaare to email notifcations

--- src/routes/api.php
+++ src/routes/api.php
@@ -10,6 +10,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/signup', [AuthController::class, 'signup']);
 Route::post('/auth/signin', [AuthController::class, 'signin']);
 
-Route::get("/verify-email/{id}/{hash}", [VerificationController::class, 'verify'])->name('verification.verify');
-
-Route::middleware(JwtMiddleware::class)->group(function () {});
+Route::get("/verify-email/{id}/{hash}", [VerificationController::class, 'verify'])
+  ->name('verification.verify')->middleware(['signed']);
