19d10ecf2acf2cdfdec4b690e488dd48cce78441
boody <abdoarfat2006@gmail.com>
2025-08-01 18:14:14 +0300

    ci: Updtate the test to don't use the middleware of Jwt In the testing time

--- src/tests/Feature/LogoutApiTest.php
+++ src/tests/Feature/LogoutApiTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Http\Middleware\JwtMiddleware;
 use App\Models\User;
 use App\Services\JwtService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -24,15 +25,24 @@ class LogoutApiTest extends TestCase
     }
     public function test_auth_logout_success()
     {
-        $user = User::factory()->create();
+        $user =  User::factory()->create();
 
         // 2. Generate JWT token
         $token = JWTAuth::fromUser($user);
+        $this->withoutMiddleware(
+            [
+                JwtMiddleware::class
+            ]
+        );
         // 3. Send request with token in cookie
         $this->withCookie('token', $token);
-
         $response = $this
             ->post('/api/auth/logout');
+        $response->assertStatus(200);
+        $response->assertJson(['message' => 'Logout successful']);
+        $response->withoutCookie('token');
+        $response->withoutCookie('refresh_token');
+
         // 4. Assert status or message
         // this work good with postman but not with phpunit because of the cookie
     }
